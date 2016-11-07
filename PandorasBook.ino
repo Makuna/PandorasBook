641f1a2c2e22fa9dc3020052b05d4cc2dc0d0b13
Michael Miller <Makuna@hotmail.com>
2016-11-06 19:54:03 -0800

    remove debug and add status blinking

--- PandorasBook.ino
+++ PandorasBook.ino
@@ -1,19 +1,25 @@
 #include <ESP8266WiFi.h>
 #include <PandoraCommunications.h>
 #include <AnalogKeypad.h>
+#include "StatusLed.h"
 
 const char* ssid     = "AP001";
 const char* password = "ItIsABadDay";
 
 PandoraClient chest;
 AnalogKeypad keypad(A0, 500);
+StatusLed<LedInvertedPinLevelFeature> statusLed(LED_BUILTIN);
 
 void setup() 
 {
-    Serial.begin(115200);
-    while (!Serial); // wait for serial attach
-    Serial.println();
 
+//    Serial.begin(115200);
+//    while (!Serial); // wait for serial attach
+//    Serial.println();
+
+    statusLed.begin();
+    statusLed.setBlinkBeat(0b1010101010101010, 250);
+    
     chest.begin(ssid, password);
 }
 
@@ -30,24 +36,22 @@ void onClientStatusChange(const PandoraClientStatus& status)
   switch (status.state)
   {
   case PandoraClientState_WifiConnecting:
-    Serial.print(".");
+    statusLed.setBlinkBeat(0b1010101010101010, 250);
     break;
     
   case PandoraClientState_WifiConnected:
-    Serial.println("Wifi Connected");
+    statusLed.setBlinkBeat(0b1010000010100000, 250);
     break;
     
   case PandoraClientState_ClientConnecting:
-    Serial.print("+");
+    statusLed.setBlinkBeat(0b1010100010101000, 250);
     break;
     
   case PandoraClientState_ClientConnected:
-    Serial.println("Client connected");
+    statusLed.stop();
     break;
 
   case PandoraClientState_DeviceReset:    
-    Serial.println("RESETTING");
-    Serial.flush();
     break;
   }
 }
@@ -55,5 +59,6 @@ void onClientStatusChange(const PandoraClientStatus& status)
 void loop() {
   chest.loop(onClientStatusChange);
   keypad.loop(onKeyEvent);
+  statusLed.loop();
 }
 
